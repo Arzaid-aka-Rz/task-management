e0d05a12d91e06f96cd587ded96192b9a9b07d94
Arzaid-aka-Rz <arzaid010103@gmail.com>
2024-10-01 22:52:11 +0530

    url change

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-  origin:'http://localhost:5173',
+  origin:'https://task-management-nyaq.onrender.com/',
   credentials:true
 }
 app.use(cors(corsOptions));
